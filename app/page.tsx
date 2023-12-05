c85d992cfeb19396c46f389f982d1ddfbd9680ef
jimmylee <send@jimmy.email>
2023-12-05 12:41:00 -0800

    AbsoluteURL example in app directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Script from 'next/script';
+import { headers } from 'next/headers';
 
 import '@root/global.scss';
 
@@ -32,6 +33,14 @@ export async function generateMetadata({ params, searchParams }) {
 }
 
 export default async function Page(props) {
+  const headersList = headers();
+  const host = headersList.get('host');
+  const protocol = headersList.get('x-forwarded-proto');
+  const absoluteURL = `${protocol}://${host}`;
+
+  const response = await fetch(`${absoluteURL}/api`);
+  const { text } = await response.json();
+
   return (
     <DefaultLayout>
       <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
@@ -46,6 +55,14 @@ export default async function Page(props) {
       </Script>
       Your objective should always be to eliminate instructions entirely by making everything self-explanatory, or as close to it as possible. When instructions are absolutely
       necessary, cut them back to a bare minimum.
+      <br />
+      <br />
+      <br />
+      Scratches
+      <br />
+      {absoluteURL}/api
+      <br />
+      API server response text: "{text}"
     </DefaultLayout>
   );
 }
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -5,5 +5,5 @@ import * as Server from '@common/server';
 export default async function apiIndex(req, res) {
   await Server.cors(req, res);
 
-  res.json({ succes: true });
+  res.json({ succes: true, text: 'hello!' });
 }
