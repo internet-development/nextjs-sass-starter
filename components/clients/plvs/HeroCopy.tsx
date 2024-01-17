8068f3ddc7494d97299270e935049a463b795029
jimmylee <send@jimmy.email>
2024-01-17 03:01:02 -0800

    1.0.0 - color tweak

--- components/clients/plvs/HeroCopy.tsx
+++ components/clients/plvs/HeroCopy.tsx
@@ -12,7 +12,7 @@ export default function HeroCopy(props) {
       <div className={styles.root}>
         <div className={styles.container}>
           <div className={styles.text}>
-            <H1 style={{ opacity: 0.6 }}>We build startups driving breakthroughs in computing</H1>
+            <H1>We build startups driving breakthroughs in computing</H1>
           </div>
           <div className={styles.border}>
             <PipelineIII width="100%" />
