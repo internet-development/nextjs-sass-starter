05dd3b164f41f4f19b7880aaa6f602523c2d1be8
jimmylee <send@jimmy.email>
2024-01-19 01:28:54 -0800

    1.0.0 - client guillotine

--- pages/clients/guillotine/index.tsx
+++ pages/clients/guillotine/index.tsx
@@ -3,10 +3,14 @@ import * as Utilities from '@common/utilities';
 
 import Page from '@components/Page';
 
+import GuillotineText from '@components/clients/guillotine/svg/GuillotineText';
+import GuillotineSymbol from '@components/clients/guillotine/svg/GuillotineSymbol';
+import HomePage from '@components/clients/guillotine/HomePage';
+
 function ClientGuillotine(props) {
   return (
     <Page title="Prototype: Guillotine" description="This is a prototype." url="https://wireframes.internet.dev/clients/guillotine">
-      Hello World
+      <HomePage />
     </Page>
   );
 }
