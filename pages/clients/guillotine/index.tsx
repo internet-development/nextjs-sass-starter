58053814f6271ac98ebc6171477795a925b4b655
jimmylee <send@jimmy.email>
2024-01-18 17:39:29 -0800

    1.0.0 - fixes build

--- pages/clients/guillotine/index.tsx
+++ pages/clients/guillotine/index.tsx
@@ -1,1 +1,20 @@
-index.tsx;
+import * as React from 'react';
+import * as Utilities from '@common/utilities';
+
+import Page from '@components/Page';
+
+function ClientGuillotine(props) {
+  return (
+    <Page title="Prototype: Guillotine" description="This is a prototype." url="https://wireframes.internet.dev/clients/guillotine">
+      Hello World
+    </Page>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {},
+  };
+}
+
+export default ClientGuillotine;
