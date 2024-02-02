20bfc13b6c1272da43c621124f914c98d0db3ad8
xBalbinus <xiangan9@gmail.com>
2024-02-01 23:18:59 -0500

    feat: deploy CI [assume autodeploy off]

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -1,11 +1,12 @@
 import styles from '@components/DefaultLayout.module.scss';
 
 import * as React from 'react';
+import Image from 'next/image';
 
 export default function App(props) {
   return (
     <div className={styles.body}>
-      <img className={styles.pixel} src={props.previewPixelSRC} />
+      <Image className={styles.pixel} src={props.previewPixelSRC} alt={''} />
       {props.children}
     </div>
   );
--- components/clients/plvs/Navigation.tsx
+++ components/clients/plvs/Navigation.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 
 import Twitter from '@system/svg/social/Twitter';
 import PLVS from '@system/svg/clients/PLVS';
+import Link from 'next/link';
 
 export default function Navigation(props) {
   return (
@@ -11,26 +12,26 @@ export default function Navigation(props) {
       <nav className={styles.navigation}>
         <div className={styles.left}>
           {!props.hideLogo ? (
-            <a className={styles.boldLinkWeb} href="/clients/plvs">
+            <Link className={styles.boldLinkWeb} href="/clients/plvs">
               <PLVS height="16px" />
-            </a>
+            </Link>
           ) : null}
           {!props.hideLogo ? (
-            <a className={styles.boldLinkMobile} href="/clients/plvs">
+            <Link className={styles.boldLinkMobile} href="/clients/plvs">
               <PLVS height="16px" />
-            </a>
+            </Link>
           ) : null}
         </div>
         <div className={styles.right}>
-          <a className={styles.link} href="/clients/plvs/portfolio">
+          <Link className={styles.link} href="/clients/plvs/portfolio">
             Portfolio
-          </a>
-          <a className={styles.link} href="/clients/plvs/people">
+          </Link>
+          <Link className={styles.link} href="/clients/plvs/people">
             People
-          </a>
-          <a className={styles.link} href="/clients/plvs/blog">
+          </Link>
+          <Link className={styles.link} href="/clients/plvs/blog">
             Blog
-          </a>
+          </Link>
           <a className={styles.link} href="https://twitter.com/protocollabs" target="_blank">
             <Twitter height="16px" width="16px" />
           </a>
