7d33d0898050581a3b1b930c7554df0d4b61bd04
jimmylee <send@jimmy.email>
2024-03-07 03:36:32 -0800

    wireframe test

---
+++ pages/clients/misc/muddy.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import HomeScreen from '@components/clients/misc/HomeScreen';
+
+function MuddyPage(props) {
+  return <HomeScreen />;
+}
+
+export default MuddyPage;
