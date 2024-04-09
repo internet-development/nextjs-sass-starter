eb3a00792d2c80d396cf64147225006aa981dfba
xBalbinus <xiangan9@gmail.com>
2024-04-09 11:44:18 -0400

    radar chart revamp

--- pages/examples/system/data-visualization/radar.tsx
+++ pages/examples/system/data-visualization/radar.tsx
@@ -53,13 +53,37 @@ const TABLE_DATA = [
 ];
 
 const EXAMPLE_DUMMY_DATA = [
-  { label: "Data 1", value: 100 },
-  { label: "Data 2", value: 200 },
-  { label: "Data 3", value: 300 },
-  { label: "Data 4", value: 400 },
-  { label: "Data 5", value: 500 },
+  [ // iPhone
+    { axis: "Battery Life", value: 0.22 },
+    { axis: "Brand", value: 0.28 },
+    { axis: "Contract Cost", value: 0.29 },
+    { axis: "Design And Quality", value: 0.17 },
+    { axis: "Have Internet Connectivity", value: 0.22 },
+    { axis: "Large Screen", value: 0.02 },
+    { axis: "Price Of Device", value: 0.21 },
+    { axis: "To Be A Smartphone", value: 0.50 }
+  ],
+  [ // Samsung
+    { axis: "Battery Life", value: 0.27 },
+    { axis: "Brand", value: 0.16 },
+    { axis: "Contract Cost", value: 0.35 },
+    { axis: "Design And Quality", value: 0.13 },
+    { axis: "Have Internet Connectivity", value: 0.20 },
+    { axis: "Large Screen", value: 0.13 },
+    { axis: "Price Of Device", value: 0.35 },
+    { axis: "To Be A Smartphone", value: 0.38 }
+  ],
+  [ // Nokia Smartphone
+    { axis: "Battery Life", value: 0.26 },
+    { axis: "Brand", value: 0.10 },
+    { axis: "Contract Cost", value: 0.30 },
+    { axis: "Design And Quality", value: 0.14 },
+    { axis: "Have Internet Connectivity", value: 0.22 },
+    { axis: "Large Screen", value: 0.04 },
+    { axis: "Price Of Device", value: 0.41 },
+    { axis: "To Be A Smartphone", value: 0.30 }
+  ]
 ];
-
 function ExampleSystemDataVisualizationRadar(props) {
   const [currentModal, setModal] = React.useState<Record<string, any> | null>(null);
   const chartContainerStyles = { padding: `0px 24px 16px 16px`, minHeight: 188 };
