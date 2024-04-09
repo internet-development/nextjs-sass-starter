efa317271b21a697109e9e7303099770c8089bb9
jimmylee <send@jimmy.email>
2024-04-08 23:24:12 -0700

    concepts

--- pages/clients/muddy/index.tsx
+++ pages/clients/muddy/index.tsx
@@ -1,6 +1,7 @@
 import styles from '@pages/clients/muddy/index.module.scss';
 
-import React from 'react';
+import * as React from 'react';
+import * as Utilities from '@common/utilities';
 
 import AnyTextHeader from '@components/AnyTextHeader';
 import Page from '@components/Page';
@@ -21,6 +22,34 @@ const Section = (props) => {
   );
 };
 
+const Message = (props) => {
+  const [date, setDate] = React.useState(new Date());
+
+  return (
+    <div className={styles.message}>
+      <div className={styles.messageLeft}>
+        <div className={styles.messageTopLine} />
+        <img className={styles.messageAvatar} src={props.src} alt="Avatar image" />
+        <figure className={styles.messageBottomLine} />
+      </div>
+      <div className={styles.messageRight}>
+        <div className={styles.messageName}>{props.name}</div>
+        <div className={styles.messageByline}>Arrakis Loop ⎯ {Utilities.timeAgo(date)}</div>
+        <div className={styles.messageContents}>{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+const Window = (props) => {
+  return (
+    <div className={styles.window}>
+      <div className={styles.windowHeader}>{props.title}</div>
+      <div className={styles.windowContent}>{props.children}</div>
+    </div>
+  );
+};
+
 function ExampleClientsMuddy(props) {
   return (
     <Page title="nextjs-sass-starter: clients: muddy" description="for Muddy" url="https://wireframes.internet.dev/clients/muddy">
@@ -29,14 +58,48 @@ function ExampleClientsMuddy(props) {
         title="Use apps with your messages"
         description="We want the viewer to know that apps open inline with messages and load instantly. You can scroll through apps just like messages. Way faster than clicking on a link and opening a tab."
       >
-        ...
-      </Section>
+        <Message name="Lady Jessica Atreides" src="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/30f8b390-a0fc-43d3-ad07-6b3efe93e9dc.png">
+          I must not fear. Fear is the mind-killer. Fear is the little death that brings obliteration. I will face my fear and I will permit it to pass over me and through me. And
+          when it has gone past... I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.
+          <Window title="https://cargo.site/">
+            <iframe src="https://cargo.site/" width="100%" height="280" frameBorder="0"></iframe>
+          </Window>
+        </Message>
 
+        <Message name="Stilgar" src="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/1534d654-0e7c-4cb1-bc4c-00b2bd9e9bb0.png">
+          Be simple, be direct.
+        </Message>
+
+        <Message name="Glossu Rabban" src="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/55d5b7ba-9b4a-4be5-916e-59be60991dba.png">
+          how did we let this happen? How can the Emperor take everything we've built and give it to Ron and Jimmy? How?
+          <Window title="https://godly.website">
+            <iframe src="https://godly.website" width="100%" height="280" frameBorder="0"></iframe>
+          </Window>
+        </Message>
+
+        <Message name="Liet Kynes" src="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/73b0e652-d11a-4178-b3e5-f691b235f877.png">
+          He felt an offense against what his mother called his instinct for rightness. It wasn’t that Reverend Mother lied to him. She obviously believed what she said. It was
+          something deeper, something tied to his terrible purpose.
+          <Window title="https://blazetype.eu/">
+            <iframe src="https://blazetype.eu/" width="100%" height="280" frameBorder="0"></iframe>
+          </Window>
+        </Message>
+      </Section>
       <Section
         title="Always available support"
         description="MuddyAI has all of my team's context and can answer questions with it, it can also browse the web and look up the latest information, and it can send those websites back to me."
       >
-        ...
+        <Message name="Glossu Rabban" src="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/55d5b7ba-9b4a-4be5-916e-59be60991dba.png">
+          I NEED A BETTER FONT. I NEED IT NOW.
+        </Message>
+
+        <Message name="Muddy Buddy" src="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/74ce6c3f-4cc2-4b7d-9fee-62226e8cb32f.jpg">
+          Okay Glossau Rabban, here is a Font from Kilm Type Foundry named Söhne. Söhne is the memory of Akzidenz-Grotesk framed through the reality of Helvetica. It captures the
+          analogue materiality of “Standard Medium” used in Unimark’s legendary wayfinding system for the NYC Subway.
+          <Window title="https://typographica.org/typeface-reviews/sohne/">
+            <iframe src="https://typographica.org/typeface-reviews/sohne/" width="100%" height="280" frameBorder="0"></iframe>
+          </Window>
+        </Message>
       </Section>
 
       <Section
