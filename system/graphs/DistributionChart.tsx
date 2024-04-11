51c5f2625f4a345fe10c48ec65d25fe28f4a4d0a
Anastasiya Uraleva <anastasiyauraleva@gmail.com>
2024-04-10 23:40:35 -0400

    add helper lines to distribution chart

--- system/graphs/DistributionChart.tsx
+++ system/graphs/DistributionChart.tsx
@@ -11,7 +11,7 @@ const DistributionChart = (props) => {
     const svg = d3.select(d3Container.current);
     svg.selectAll('*').remove();
 
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
     const height = 400 - margin.top - margin.bottom;
     const chartWidth = width - margin.left - margin.right;
 
@@ -23,11 +23,22 @@ const DistributionChart = (props) => {
     xScale.domain(props.data.map((d) => d.label));
     yScale.domain([0, d3.max(props.data, (d) => d.value)]);
 
-    g.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(xScale));
+    const tickValues = yScale.ticks();
 
-    g.append('g').call(d3.axisLeft(yScale));
+    tickValues.forEach((d) => {
+      g.append('line').attr('x1', 0).attr('x2', chartWidth).attr('y1', yScale(d)).attr('y2', yScale(d)).attr('stroke', 'var(--color-border)').attr('opacity', 0.7); // Adjust opacity as needed
+    });
+
+    const yAxis = g.append('g').call(d3.axisLeft(yScale));
+    yAxis.selectAll('.tick text').style('fill', 'var(--color-border)').style('font-size', '16px');
+    yAxis.selectAll('.tick line, .domain').style('stroke', 'var(--color-border)');
+
+    const xAxisOffset = 0.5;
+    const xAxis = g
+      .append('g')
+      .attr('transform', `translate(0,${height + xAxisOffset})`)
+      .call(d3.axisBottom(xScale));
+    xAxis.selectAll('text').style('font-size', '14px');
 
     // Draw bars
     g.selectAll('.distribution')
@@ -40,17 +51,13 @@ const DistributionChart = (props) => {
       .attr('width', xScale.bandwidth())
       .attr('fill', 'rgba(106, 243, 168, 0.3)');
 
-    const line = d3.line()
+    const line = d3
+      .line()
       .curve(d3.curveMonotoneX)
       .x((d) => xScale(d.label) + xScale.bandwidth() / 2)
       .y((d) => yScale(d.value));
 
-    g.append("path")
-      .datum(props.data)
-      .attr("fill", "none")
-      .attr("stroke", "green")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
+    g.append('path').datum(props.data).attr('fill', 'none').attr('stroke', 'green').attr('stroke-width', 1.5).attr('d', line);
   };
 
   useEffect(() => {
