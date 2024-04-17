8098bd940284119ce4ba5a1080383bd805186f40
jimmylee <send@jimmy.email>
2024-04-17 01:23:40 -0700

    colors order adjustment

--- pages/examples/system/colors.tsx
+++ pages/examples/system/colors.tsx
@@ -18,15 +18,8 @@ const TABLE_HEADINGS = [``, `CSS Variable`, `Preview`];
 const COPY = `My heart had great experience of wisdom and knowledge, [1:17] and I gave my heart to know wisdom, and to know madness and folly: I perceived that this also is vexation of spirit. For in much wisdom is much grief: and he that increaseth knowledge increaseth sorrow.`;
 
 const TABLE_DATA = [
-  'var(--theme-background)',
-  'var(--theme-background-overlay)',
-  'var(--theme-background-box-top)',
-  'var(--theme-background-box-side)',
-  'var(--theme-background-box-front)',
   'var(--theme-foreground)',
   'var(--theme-foreground-secondary)',
-  'var(--theme-button)',
-  'var(--theme-button-text)',
   'var(--theme-text)',
   'var(--theme-border)',
   'var(--theme-border-subdued)',
@@ -36,6 +29,13 @@ const TABLE_DATA = [
   'var(--theme-error-subdued)',
   'var(--theme-success)',
   'var(--theme-success-subdued)',
+  'var(--theme-button)',
+  'var(--theme-button-text)',
+  'var(--theme-background)',
+  'var(--theme-background-overlay)',
+  'var(--theme-background-box-top)',
+  'var(--theme-background-box-side)',
+  'var(--theme-background-box-front)',
 ].map((backgroundColor) => {
   return { data: [``, <Tag>{backgroundColor}</Tag>, <ImageBlock style={{ backgroundColor }} />] };
 });
