3f12bd50424d2285153d378f136bb4f57ec30ac3
Elijah Seed-Arita <elijaharita@gmail.com>
2024-08-22 21:39:19 -0400

    replace face example quotes

--- pages/examples/animations/fade.tsx
+++ pages/examples/animations/fade.tsx
@@ -43,46 +43,52 @@ function ExampleFade(props) {
       <div style={{ width: 648, margin: '64px auto' }}>
         <FadeManager key={nonce} {...fadeConfig}>
           <BlockFade>
-            <H1>Placeholder Title</H1>
+            <H1>Heading 1</H1>
           </BlockFade>
           <BlockFade>
             <P>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula
-              venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa.
+              The solar system is situated in a region of the universe, every point of which has a common and constant temperature, determined by the rays of light and heat which
+              proceed from the surrounding stars. This low temperature is a little below that of the polar regions of the earth.
             </P>
           </BlockFade>
           <P>
             <br />
           </P>
           <BlockFade>
-            <H2>Subheading Example</H2>
+            <H2>Heading 2</H2>
           </BlockFade>
           <BlockFade>
             <P>
-              Fusce lacinia arcu et nulla. Nulla vitae mauris non felis mollis faucibus. Phasellus consectetur, orci sit amet ullamcorper imperdiet, turpis ligula facilisis urna,
-              vel fringilla libero mauris in urna.
+              The earth would have only the same temperature as the heavens, were it not for two causes... One is the internal heat... possessed at its formation... only dissipated
+              through the surface; the other is the continued action of the solar rays... which produce at the surface, the diversities of climate.
             </P>
           </BlockFade>
           <P>
             <br />
           </P>
           <BlockFade>
-            <H3>Another Subheading</H3>
+            <H3>Heading 3</H3>
           </BlockFade>
           <BlockFade>
             <P>
-              Integer in volutpat libero. Vivamus vehicula aliquet ligula, non aliquet justo maximus non. Phasellus nec mi id eros suscipit auctor. In ut purus et lectus faucibus
-              ultricies sit amet non ex.
+              Liquids are very poor conductors of heat; but they have, like aeriform media, the property of carrying it rapidly in certain directions. This is the same property
+              which, combining with, combining with the centrifugal force, displaces and mingles all parts of the atmosphere... [and] ocean, and maintains in them, regular and
+              immense currents.
             </P>
           </BlockFade>
           <P>
             <br />
           </P>
           <BlockFade>
-            <H4>Minor Heading</H4>
+            <H4>Heading 4</H4>
           </BlockFade>
           <BlockFade>
-            <P>Quisque at sapien ut justo facilisis pharetra. Integer euismod nunc vel orci fermentum, id tincidunt neque condimentum. Suspendisse potenti.</P>
+            <P>
+              The interposition of the air very much modifies the effects of the heat upon the surface of the globe. The solar rays traversing the atmospheric strata, which are
+              condensed by their own weight [at decreasing altitudes], heat them very unequally; those which are rarest are likewise coldest, because they... absorb a smaller part
+              of the rays. The heat of the sun... in the form of light, possesses the property of penetrating transparent solids or liquids, and loses this property... when by...
+              terrestrial bodies, it is turned into heat radiating without light.
+            </P>
           </BlockFade>
           <P>
             <br />
