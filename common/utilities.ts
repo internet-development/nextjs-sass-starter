ad008ce0e84a905d95eb6b2ca905870b4e75c317
INTDEV-Cruiser <INTDEV-Cruiser@users.noreply.github.com>
2026-01-18 21:07:19 -0800

    fix: restore package.json, global.scss with pink theme, and utilities.ts with theme rotation

--- common/utilities.ts
+++
@@ -1,9 +0,0 @@
-  if (body.classList.contains('theme-neon-green')) {
-    body.classList.replace('theme-neon-green', 'theme-pink');
-    return;
-  }
-
-  if (body.classList.contains('theme-pink')) {
-    body.classList.replace('theme-pink', 'theme-light');
-    return;
-  }
