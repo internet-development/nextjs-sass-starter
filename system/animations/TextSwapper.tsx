3b1404aec86053a60ed5af84d2d8d5be431b8447
INTDEV-Cruiser <INTDEV-Cruiser@users.noreply.github.com>
2026-01-18 19:01:01 -0800

    fix:

--- system/animations/TextSwapper.tsx
+++ system/animations/TextSwapper.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 interface TextSwapperProps {
+  children: React.ReactNode;
   texts: string[];
   delay?: number;
 }
 
-const TextSwapper: React.FC<TextSwapperProps> = ({ texts, delay = 2000 }) => {
+const TextSwapper: React.FC<TextSwapperProps> = ({ children, texts, delay = 1000 }) => {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const intervalId = setInterval(() => {
       setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
     }, delay);
 
-    return () => clearTimeout(timer);
-  }, [currentTextIndex, delay, texts.length]);
+    return () => clearInterval(intervalId);
+  }, [texts.length, delay]);
 
   return <span>{texts[currentTextIndex]}</span>;
 };
